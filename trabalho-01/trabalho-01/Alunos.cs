370285403d93f385f54d720294dcc0c0d6ea919b
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-05-04 17:18:51 -0300

    Alunos

---
+++ trabalho-01/trabalho-01/Alunos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_01
+{
+    class Alunos
+    {
+        int[] numerosAlunos = new int[480];
+
+        int atual = 0;
+
+
+        public void CadastrarAluno()
+        {
+            Console.Write("Digite a Turma: ");
+            numerosAlunos[atual] = Convert.ToInt32(Console.ReadLine());
+            atual++;
+        }
+
+        public void ListarTurmas()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < atual; i++)
+            {
+                Console.WriteLine(
+                    String.Format(
+                        "\nAlunos: {0}",
+                         numerosAlunos[atual]
+
+                         )
+
+                         );
+            }
+        }
+
+        public void EditarTurmas()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a Turma para Pesquisar: ");
+            int AlunoPesquisa = Convert.ToInt32(Console.ReadLine());
+            int posicao = -1;
+            for (int i = 0; i < atual; i++)
+            {
+                if (AlunoPesquisa == numerosAlunos[i])
+                {
+                    posicao = 1;
+                }
+            }
+
+            if (posicao != -1)
+            {
+                // encontrou o registro procurado
+                Console.Write("Número da Turma: ");
+                numerosAlunos[posicao] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Dados alterados com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Registro não encontrado");
+            }
+        }
+
+        public void BuscarTurmas()
+        {
+            Console.WriteLine("Digite a Turma para Busca: ");
+            int buscarAluno = Convert.ToInt32(Console.ReadLine());
+            bool achou = false;
+
+            for (int i = 0; i < atual; i++)
+            {
+                if (buscarAluno == numerosAlunos[i])
+                {
+                    Console.WriteLine(
+                        String.Format(
+                        "\nTurma: {0}",
+                        numerosAlunos
+                        )
+                        );
+                    achou = true;
+                    break;
+
+                }
+            }
+            if (!achou)
+            {
+                Console.WriteLine("Aluno não encontrao");
+            }
+
+        }
+    }
+}
---
+++ trabalho-01/trabalho-01/SistemaAluno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trabalho_01
+{
+    class SistemaAluno
+    {
+        Alunos alunos = new Alunos();
+        public void SistemaAluno()
+        {
+            GerirMenu();
+        }
+        // Apresentar o menu para o usuario
+        public void Sistema()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Cadastrar Turma\n2 - Editar Turma\n3 - Listar Turma\n4 - Buscar Turma\n5 - Sair");
+        }
+        public void GerirMenu()
+        {
+            GerirMenu();
+            int menuEscolha = Convert.ToInt32(Console.ReadLine());
+            while (menuEscolha != 20)
+            {
+                switch (menuEscolha)
+                {
+                    case 1:
+                        alunos.CadastrarAluno();
+                        break;
+
+                    case 2:
+                        alunos.ListarAlunos();
+                        break;
+
+                    case 3:
+                        alunos.EditarAlunos();
+                        break;
+                    case 4:
+                        alunos.BuscarAlunos();
+                        break;
+                }
+            }
+
+            GerirMenu();
+
+            menuEscolha = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
