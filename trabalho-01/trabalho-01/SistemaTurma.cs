b4c8613203042ff0e69c2f9727191faea648d70c
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-05-04 15:22:03 -0300

    Alunos

---
+++ trabalho-01/trabalho-01/SistemaTurma.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trabalho_01
+{
+    class SistemaTurma
+    {
+        Turmas turmas = new Turmas();
+        public SistemaTurma()
+        {
+            GerirMenu();
+        }
+        // Apresentar o menu para o usuario
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Cadastrar Turma\n2 - Editar Turma\n3 - Listar Turma\n4 - Buscar Turma\n5 - Sair");
+        }
+       public void GerirMenu()
+        {
+            /*
+            *  Chama método para apresentar o menu para o usuario
+            */
+            Menu();
+            int menuEscolha = Convert.ToInt32(Console.ReadLine());
+            while (menuEscolha != 20)
+            {
+                switch (menuEscolha)
+                {
+                    case 1:
+                        turmas.CadastrarTurmas();
+                        break;
+
+                    case 2:
+                        turmas.ListarTurmas();
+                        break;
+
+                    case 3:
+                        turmas.EditarTurmas();
+                        break;
+                    case 4:
+                        turmas.BuscarTurmas();
+                        break;
+                }
+
+                Menu();
+
+                menuEscolha = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ trabalho-01/trabalho-01/Turmas.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_01
+{
+    class Turmas
+    {
+        int[] numerosTurmas = new int[16];
+
+        int atual = 0;
+
+
+        public void CadastrarTurmas()
+        {
+            Console.Write("Digite a Turma: ");
+            numerosTurmas[atual] = Convert.ToInt32(Console.ReadLine());
+            atual++;
+        }
+
+        public void ListarTurmas()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < atual; i++)
+            {
+                Console.WriteLine(
+                    String.Format(
+                        "\nTurma: {0}",
+                         numerosTurmas[atual]
+                         
+                         )
+                         
+                         );
+            }
+        }
+            
+         public void EditarTurmas()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a Turma para Pesquisar: ");
+            int turmaPesquisa = Convert.ToInt32(Console.ReadLine());
+            int posicao = -1;
+            for (int i = 0; i < atual; i++)
+            {
+                if (turmaPesquisa == numerosTurmas[i])
+                {
+                    posicao = 1;
+                }
+            }
+
+            if (posicao != -1)
+            {
+                // encontrou o registro procurado
+                Console.Write("Número da Turma: ");
+                numerosTurmas[posicao] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Dados alterados com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Registro não encontrado");
+            }
+        }
+        public void BuscarTurmas()
+            {
+                Console.WriteLine("Digite a Turma para Busca: ");
+                int buscarTurma = Convert.ToInt32(Console.ReadLine());
+                bool achou = false;
+
+                for (int i = 0; i < atual; i++)
+			    {
+			        if (buscarTurma == numerosTurmas[i])
+                    {
+                        Console.WriteLine(
+                            String.Format(
+                            "\nTurma: {0}", 
+                            numerosTurmas
+                            )
+                            );
+                            achou = true;
+                            break;
+
+                    }
+			    }
+                if (!achou)
+	            {
+                   Console.WriteLine("Nome não encontrao");
+                }
+
+            }
+    }
+}
+
