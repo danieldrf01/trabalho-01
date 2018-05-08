403e2df952a77f4ca52ad3db8ca0f324cb8cf4d0
Daniel Rodrigo França <danieldrf01@gmail.com>
2018-05-08 14:38:12 -0300

    Sistema Notas

--- trabalho-01/trabalho-01/SistemaNota.cs
+++ trabalho-01/trabalho-01/SistemaNota.cs
@@ -7,6 +7,48 @@ namespace trabalho_01
 {
     class SistemaNota
     {
+        Notas notas = new Notas();
+        public void SistemaNota()
+        {
+            GerirMenu();
+        }
+        public void Sistema()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Cadastar Nota:\n2 - Listar Nota\n3 - Editar Nota\n4 - Buscar Nota\n5 - Sair");
+        }
+        public void GerirMenu()
+        {
+            GerirMenu();
+            int menuEscolha = Convert.ToInt32(Console.ReadLine());
+            
+            while(menuEscolha != 5)
+            {
+                switch (menuEscolha)
+                {
+                    case 1:
+                        notas.CadastrarNotas();
+                        break;
+
+                    case 2:
+                        notas.ListarNotas();
+                        break;
+
+                    case 3:
+                        notas.EditarNota();
+                        break;
+
+                    case 4:
+                        notas.BuscarNota();
+                        break;
+
+                }
+            }
+
+            GerirMenu();
+            menuEscolha = Convert.ToInt32(Console.ReadLine());
+
+        }
 
     }
 }
